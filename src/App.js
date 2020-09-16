923b0f8816bdf7abb5e2aa225e57d6d093a79b8c
abhay singh <a8haysingh@gmail.com>
2020-09-16 17:58:10 +0530

    created home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles.css";
 import Header from "./Header";
+import Home from "./Home";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
